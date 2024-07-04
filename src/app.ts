c3cad304066bcec91fd274fb182db321825dcbbe
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2024-07-03 20:39:38 -0400

    ⚠️ Warn if installed locally

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import { Page } from './class/pages.js'
 import { page } from './index.js'
+import isGlobal from 'is-installed-globally'
+import c from 'chalk'
+
+if (!isGlobal) {
+  console.log(`⚠️  ${c.red('Warning')}!`)
+  console.log(`     It is ${c.bgRed(c.black('not recommended'))} to use this module locally, ${c.bgGreen('install it globally')} with ${c.italic.underline('npm i -g cloudflare-manager')}\n`)
+}
 
 await Page.register()
 const cachePage = Page.all.find((pagee) => pagee.interaction.name === page.get())
